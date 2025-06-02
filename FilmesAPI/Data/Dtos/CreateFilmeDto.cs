d93f422407b3637f8931a4e68bed54152f4a8e01
Mateus <Mateus@Desktop-CMZFO>
2025-06-02 18:15:24 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateFilmeDto
+{
+
+    [Required(ErrorMessage = "O titulo e obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero e obrigatorio")]
+    [StringLength(50, ErrorMessage = "Tamanho maximo de 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A duracao e obrigatorio")]
+    [Range(70, 600, ErrorMessage = "A duracao preciisa ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{   
+    [Required]
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo e obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero e obrigatorio")]
+    [StringLength(50, ErrorMessage = "Tamanho maximo de 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A duracao e obrigatorio")]
+    [Range(70, 600, ErrorMessage = "A duracao preciisa ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
